dc6de1352fe961c121a8b35472740f2c2d33993f
fahadrajpoot537 <143400548+fahadrajpoot537@users.noreply.github.com>
2025-02-24 23:44:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ link.forEach(function (alinks) {
     mouse.style.scale = 3;
   });
   alinks.addEventListener("mouseleave", () => {
-    mouse.style.backgroundColor = "#ffa500";
+    mouse.style.backgroundColor = "#550000";
     mouse.style.border = "none";
     mouse.style.scale = 1;
   });
